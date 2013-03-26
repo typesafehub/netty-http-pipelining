354cf0a6340150cd3875ca87cb713aa2eed04829
Christopher Hunt <huntchr@gmail.com>
2013-03-26 15:32:00 +1100

    Introduced a guard in terms of the maximum number of messages that will be held back. Therefore if something is taking too long then we'll disconnect the channel.

--- src/main/java/org/typesafe/netty/http/pipelining/HttpPipeliningHandler.java
+++ src/main/java/org/typesafe/netty/http/pipelining/HttpPipeliningHandler.java
@@ -10,25 +10,46 @@ import java.util.PriorityQueue;
 
 /**
  * Implements HTTP pipelining ordering, ensuring that responses are completely served in the same order as their
- * corresponding requests.
+ * corresponding requests. This class is optimised given the use of BufferedWriteHandler. Messages are flushed to the
+ * network buffer with a minimal number of operations.
  *
  * @author Christopher Hunt
  */
 public class HttpPipeliningHandler extends BufferedWriteHandler {
 
+    public static final int MAX_EVENTS_HELD = 10000;
+
+    private final int maxEventsHeld;
+
     private int sequence;
     private int nextRequiredSequence;
 
-    private final PriorityQueue<OrderedDownstreamMessageEvent> holdingQueue =
-            new PriorityQueue<OrderedDownstreamMessageEvent>(100, new Comparator<OrderedDownstreamMessageEvent>() {
-                @Override
-                public int compare(OrderedDownstreamMessageEvent o1, OrderedDownstreamMessageEvent o2) {
-                    return o1.getSequence() - o2.getSequence();
-                }
-            });
+    private final PriorityQueue<OrderedDownstreamMessageEvent> holdingQueue;
 
     public HttpPipeliningHandler() {
+        this(MAX_EVENTS_HELD);
+    }
+
+    /**
+     * @param maxEventsHeld the maximum number of message events that will be retained prior to aborting the channel
+     *                      connection. This is required as events cannot queue up indefintely; we would run out of
+     *                      memory if this was the case.
+     */
+    public HttpPipeliningHandler(final int maxEventsHeld) {
         super(true);
+
+        this.maxEventsHeld = maxEventsHeld;
+
+        holdingQueue = new PriorityQueue<OrderedDownstreamMessageEvent>(maxEventsHeld, new Comparator<OrderedDownstreamMessageEvent>() {
+            @Override
+            public int compare(OrderedDownstreamMessageEvent o1, OrderedDownstreamMessageEvent o2) {
+                return o1.getSequence() - o2.getSequence();
+            }
+        });
+    }
+
+    public int getMaxEventsHeld() {
+        return maxEventsHeld;
     }
 
     @Override
@@ -53,20 +74,27 @@ public class HttpPipeliningHandler extends BufferedWriteHandler {
     @Override
     public synchronized void writeRequested(final ChannelHandlerContext ctx, final MessageEvent e) throws Exception {
         if (e instanceof OrderedDownstreamMessageEvent) {
-            final OrderedDownstreamMessageEvent currentEvent = (OrderedDownstreamMessageEvent) e;
-            holdingQueue.add(currentEvent);
+            if (holdingQueue.size() < maxEventsHeld) {
 
-            while (!holdingQueue.isEmpty() && holdingQueue.peek().getSequence() == nextRequiredSequence) {
-                final OrderedDownstreamMessageEvent nextEvent = holdingQueue.poll();
-                super.writeRequested(ctx, nextEvent);
-                if (nextEvent.isLast()) {
-                    ++nextRequiredSequence;
+                final OrderedDownstreamMessageEvent currentEvent = (OrderedDownstreamMessageEvent) e;
+                holdingQueue.add(currentEvent);
+
+                while (!holdingQueue.isEmpty() && holdingQueue.peek().getSequence() == nextRequiredSequence) {
+                    final OrderedDownstreamMessageEvent nextEvent = holdingQueue.poll();
+                    super.writeRequested(ctx, nextEvent);
+                    if (nextEvent.isLast()) {
+                        ++nextRequiredSequence;
+                    }
                 }
+
+                if (e.getChannel().isWritable()) {
+                    flush();
+                }
+
+            } else {
+                Channels.disconnect(e.getChannel());
             }
 
-            if (e.getChannel().isWritable()) {
-                flush();
-            }
         }
     }
 
